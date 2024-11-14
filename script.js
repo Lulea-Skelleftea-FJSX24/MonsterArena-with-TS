aacdabd0d4d322cb9bdecaf2b7b45501208dd419
Fredrik Andersson <fredrik.andersson1@chasacademy.se>
2024-11-14 14:48:19 +0100

    adderat monster

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ fetch("./monsters.json")
     data.forEach((character) => {
       const characterDiv = document.createElement("div");
       characterDiv.innerHTML = `
+       <img src="/" alt="bild på Monster">
        <h2>${character.name}</h2>
        <p><strong>Speciality:</strong> ${character.speciality}</p>
+       <button>Välj mig!</button>
      `;
       characterList.appendChild(characterDiv);
     });
